0277ed624b8b7564815d8fcc1bba7a3190af696c
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2013-07-11 18:27:48 -0400

    basic auto generator in action

--- cbits/doubleUnvect.c
+++ cbits/doubleUnvect.c
@@ -1,5 +1,5 @@
 #include "simd.h"
-#include <math.h>
+#include <tgmath.h>
 
 
 /*
@@ -20,22 +20,35 @@ because you really shouldn't do more than 4gb of work in one sequential ffi call
 */
 
 /* CPP macro to generate declarations and body for strided scalar versions */
+#define negate(numexp)  (-(numexp))
+#define reciprocal(numexp) (1.0/(numexp))
 
-#define BinaryOpScalarArray(name,type,binaryop) void name(int length, type  * restrict  left, \
+
+#define mkNumFracOpsScalar(type)  \
+BinaryOpScalarArray(arrayGeneralPlus,+,type) \
+BinaryOpScalarArray(arrayGeneralMinus,-,type) \
+BinaryOpScalarArray(arrayGeneralTimes,*,type) \
+BinaryOpScalarArray(arrayGeneralDivide,/,type) \
+UnaryOpScalarArray(arrayGeneralNegate,negate,type) \
+UnaryOpScalarArray(arrayGeneralAbs,fabs,type) \
+UnaryOpScalarArray(arrayGeneralReciprocal,reciprocal,type) \
+
+
+#define BinaryOpScalarArray(name,binaryop,type) void  name##type(int length, type  * restrict  left, \
 int leftStride ,type  * restrict  right,int rightStride, type * restrict  result, \
 int resultStride  ); \
  \
-void name(int length, double  * restrict  left,int leftStride ,double  * restrict  right,int rightStride, double * restrict  result, int resultStride  ){ \
+void name##type(int length, double  * restrict  left,int leftStride ,double  * restrict  right,int rightStride, double * restrict  result, int resultStride  ){ \
     int ix = 0 ;  \
     for (ix = 0; ix < length ; ix ++){ \
         result[ix* resultStride]= (left[ix*leftStride] ) binaryop (right[ix*rightStride]  ) ; \
     } \
 }
 
-#define UnaryOpScalarArray(name,op,type) void name(int length, type * restrict in,int inStride,\
+#define UnaryOpScalarArray(name,op,type) void name##type(int length, type * restrict in,int inStride,\
 double * restrict out, int outStride); \
  \
-void name(int length, double * restrict in,int inStride, double * restrict out, \
+void name##type(int length, double * restrict in,int inStride, double * restrict out, \
 int outStride){ \
     int ix = 0 ; \
     for(ix = 0 ; ix < length ; ix ++){ \
@@ -54,40 +67,25 @@ for now I shall assume that everything only works on < 4gb / 32 sized ranges
 also, will try to write things so that if any of the read (input) arrays
 */
 
-inline static  double negateDouble(double in);
-inline  static double negateDouble(double in){
-    return -in ; 
-}
+// inline static  double negateDouble(double in);
+// inline  static double negateDouble(double in){
+//     return -in ; 
+// }
 
-inline static  float negateFloat(float in);
-inline  static float negateFloat(float in){
-    return -in ; 
-}
-inline static  double reciprocalDouble(double in);
-inline  static double reciprocalDouble(double in){
-    return 1.0/in ; 
-}
+// inline static  float negateFloat(float in);
+// inline  static float negateFloat(float in){
+//     return -in ; 
+// }
+// inline static  double reciprocalDouble(double in);
+// inline  static double reciprocalDouble(double in){
+//     return 1.0/in ; 
+// }
 
-inline static  float reciprocalDouble(float in);
-inline  static float reciprocalDouble(float in){
-    return 1.0/in ; 
-}
-
-
+// inline static  float reciprocalDouble(float in);
+// inline  static float reciprocalDouble(float in){
+//     return 1.0/in ; 
+// }
 
 
-BinaryOpScalarArray(arrayGeneralPlusDoubleM,+,double)
 
-BinaryOpScalarArray(arrayGeneralMinusDoubleM,-,double)
-
-BinaryOpScalarArray(arrayGeneralTimesDoubleM,*,double)
-
-BinaryOpScalarArray(arrayGeneralDivideDoubleM,/,double)
-
-UnaryOpScalarArray(arrayGeneralNegateDoubleM,negateDouble,double)
-
-UnaryOpScalarArray(arrayGeneralAbsDoubleM,abs,double)
-
-UnaryOpScalarArray(arrayGeneralReciprocalDoubleM,reciprocalDouble,double)
-
-
+mkNumFracOpsScalar(double) ; 
