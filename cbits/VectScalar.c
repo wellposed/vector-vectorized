b3fdd1b599da2d3d296914d95a3354e584e703d9
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2013-07-12 17:18:55 -0400

    Revert "rightnow its generating VERY bad code"
    This reverts commit eb1a3e5616e663a6e0b9573a55cb643d308ba5fb.

--- cbits/VectScalar.c
+++ cbits/VectScalar.c
@@ -1,7 +1,7 @@
 #include "simd.h"
 #include <tgmath.h>
 #include <complex.h>
-#include <stdint.h>
+
 /*
 we have 3 versions: 
 avx2(untested)
@@ -17,12 +17,6 @@ plus scalar fallback
 note, for now using 32bit int types for array operation sizes and strides 
 because you really shouldn't do more than 4gb of work in one sequential ffi call!
 
-
-Assumption: 
-stride is never zero
-
-stride
-
 */
 
 /* CPP macro to generate declarations and body for strided scalar versions */
@@ -37,14 +31,8 @@ BinaryOpScalarArray(arrayGeneralTimes,*,type) \
 BinaryOpScalarArray(arrayGeneralDivide,/,type) \
 UnaryOpScalarArray(arrayGeneralNegate,negate,type) \
 UnaryOpScalarArray(arrayGeneralAbs,fabs,type) \
-UnaryOpScalarArray(arrayGeneralReciprocal,reciprocal,type)  \
+UnaryOpScalarArray(arrayGeneralReciprocal,reciprocal,type) \
 
-
-#define AVX_Modulus (4 * sizeof(double))  
- // 4 * 64bits = 256
-#define allEqualStride(val,a,b,c) (((val )== ((long int)a)) && ((val)== ((long int)b)) && ((val)== ((long int)c)) ) 
-
-#define isAligned(x)  !((uintptr_t)x & 0xFF)
 
 #define BinaryOpScalarArray(name,binaryop,type) void  name##_##type(int length, type  * restrict  left, \
 int leftStride ,type  * restrict  right,int rightStride, type * restrict  result, \
@@ -52,25 +40,8 @@ int resultStride  ); \
  \
 void name##_##type(int length, type  * restrict  left,int leftStride ,type  * restrict  right,int rightStride, type * restrict  result, int resultStride  ){ \
     int ix = 0 ;  \
-    if(allEqual(1,leftStride,rightStride,resultStride) && isAligned(left) && isAligned(right) && isAligned(result)  ){ \
-        int quotient =  length / 8 ; \
-        int rem = length % 8 ; \
-        int octMax =  8 * rem ; \
-                \
-        for(; ix < octMax ;){ \
-            int counter = 0  ; \
-            for(; counter < 8 ; counter ++ ){ \
-                result[ix* resultStride]= (left[ix*leftStride] ) binaryop (right[ix*rightStride]  ) ; \
-                ix++  ; \
-            }} \
-            for(; ix < length ; ix ++){ \
-                result[ix* resultStride]= (left[ix*leftStride] ) binaryop (right[ix*rightStride]  ) ; \
-            } \
-        } \ 
-    else{ \
-        for(; ix < length ; ix ++){ \
-            result[ix* resultStride]= (left[ix*leftStride] ) binaryop (right[ix*rightStride]  ) ; \
-            } \
+    for (ix = 0; ix < length ; ix ++){ \
+        result[ix* resultStride]= (left[ix*leftStride] ) binaryop (right[ix*rightStride]  ) ; \
     } \
 }
 
@@ -80,7 +51,7 @@ type * restrict out, int outStride); \
 void name##_##type(int length, type * restrict in,int inStride, type * restrict out, \
 int outStride){ \
     int ix = 0 ; \
-    for(; ix < length ; ix ++){ \
+    for(ix = 0 ; ix < length ; ix ++){ \
         out[ix*outStride] = op(in[ix*inStride]); \
     } \
 }
