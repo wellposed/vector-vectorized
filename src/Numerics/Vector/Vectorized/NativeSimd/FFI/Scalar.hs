daeb8501292ca23fdf6cc8ccaae7777cd15571fc
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2013-08-20 15:18:18 -0400

    starting writing the FFI machinery

---
+++ src/Numerics/Vector/Vectorized/NativeSimd/FFI/Scalar.hs
@@ -0,0 +1,12 @@
+
+module Numerics.Vector.Vectorized.NativeSimd.FFI.Scalar where
+
+import Foreign hiding (unsafePerformIO)
+import Foreign.C.Types
+--import Unsafe.Coerce
+import Prelude hiding (replicate)
+--import Data.Storable
+--import System.IO.Unsafe
+--import Data.Vector.Storable.Mutable  
+import GHC.Ptr (castPtr)
+
