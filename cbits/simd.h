1ace63d0f40c6d57231f78305d9a634b9b923e87
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2013-09-03 17:36:54 -0400

    clean up the language in the warnings  in simd.h

--- cbits/simd.h
+++ cbits/simd.h
@@ -3,17 +3,30 @@
 #include <stddef.h>
 #include <stdint.h>
 
+
+
 #if __STDC_VERSION__ >= 199901L
   /* yay, C99 or newer!*/
 #else
-#warning "C99 or newer C standard compliant compiler needed"
+#warning "C99 or newer C standard compliant compiler is strongly recommended"
 #endif
 
 
+/*
+       #if __GNUC__ > 3 || \
+              (__GNUC__ == 3 && (__GNUC_MINOR__ > 2 || \
+                                 (__GNUC_MINOR__ == 2 && \
+                                  __GNUC_PATCHLEVEL__ > 0))
 
-#ifdef __clang__
-  /* yay, Clang!*/
-#else
-#warning "Clang is the only supported C compiler for now"
+*/
+#if defined(__clang__)
+  /* yay, Clang! */
+#elif defined(__GNUC__) && __GNUC__ > 4 || \
+              (__GNUC__ == 4 && (__GNUC_MINOR__ > 4 || \
+                                 (__GNUC_MINOR__ == 4 && \
+                                  __GNUC_PATCHLEVEL__ >=  0))
+/* yay recent gcc*/
+#else 
+#warning "Clang is the only supported C compiler for now, but recent GCC should be fine too"
 #endif
 
